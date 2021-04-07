cbf5b27c1a1dcd79841b41b7126e78038a59d74d
0f1c04 <f1c04@kakao.com>
2021-04-07 17:11:45 +0900

    #day37 TIL: JavaScript ES6 study #3

---
+++ web/day37/js/event.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    document.querySelector("#open").onclick = function () {
+        document.querySelector("#desc").style.display="block";
+        this.style.display="none";
+    }
+
+    document.querySelector("#close").onclick = function () {
+        document.querySelector("#desc").style.display="none";
+        document.querySelector("#open").style.display="block";
+    }
+}
